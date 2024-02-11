b605b7543ae4f702f0c5c849e077b30538483a6f
rei-takahashi <xxx@users.noreply.github.com>
2024-02-11 12:39:17 +0900

    トップバーとサイドバー完了

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,70 @@
+import { Bookmark, Home, MessageRounded, Notifications, Person, Search, Settings } from '@mui/icons-material'
+import React from 'react'
+import "./Sidebar.css"
+
+export default function Sidebar() {
+  return (
+    <div className='sidebar'>
+        <div className="sidebarWrapper">
+            <ul className="sidebarList">
+                <li className="sidebarListItem">
+                    <Home className='sidebarIcon'/>
+                    <span className="sidebarListItemText">ホーム</span>
+                </li>
+                <li className="sidebarListItem">
+                    <Search className='sidebarIcon'/>
+                    <span className="sidebarListItemText">検索</span>
+                </li>
+                <li className="sidebarListItem">
+                    <Notifications className='sidebarIcon'/>
+                    <span className="sidebarListItemText">通知</span>
+                </li>
+                <li className="sidebarListItem">
+                    <MessageRounded className='sidebarIcon'/>
+                    <span className="sidebarListItemText">メッセージ</span>
+                </li>
+                <li className="sidebarListItem">
+                    <Bookmark className='sidebarIcon'/>
+                    <span className="sidebarListItemText">ブックマーク</span>
+                </li>
+                <li className="sidebarListItem">
+                    <Person className='sidebarIcon'/>
+                    <span className="sidebarListItemText">プロフィール</span>
+                </li>
+                <li className="sidebarListItem">
+                    <Settings className='sidebarIcon'/>
+                    <span className="sidebarListItemText">設定</span>
+                </li>
+            </ul>
+            {/** 横線を入れる */}
+            <hr className="sidebarHr" />
+            <ul className="sidebarFriendList">
+                <li className="sidebarFriend">
+                    <img
+                        src='/assets/person/2.jpeg'
+                        alt=''
+                        className='sidebarFriendImg'   
+                    />
+                    <span className="sidebarFriendName">Shin Code</span>
+                </li>
+                <li className="sidebarFriend">
+                    <img
+                        src='/assets/person/3.jpeg'
+                        alt=''
+                        className='sidebarFriendImg'   
+                    />
+                    <span className="sidebarFriendName">俺</span>
+                </li>
+                <li className="sidebarFriend">
+                    <img
+                        src='/assets/person/4.jpeg'
+                        alt=''
+                        className='sidebarFriendImg'   
+                    />
+                    <span className="sidebarFriendName">はたきん</span>
+                </li>
+            </ul>
+        </div>
+    </div>
+  )
+}
